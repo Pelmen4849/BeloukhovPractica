3f3378215d0b673bbf894522d29f8f7fcd06fb2e
Pelmen4849 <151121401+Pelmen4849@users.noreply.github.com>
2023-11-17 13:06:24 +0300

    Задание 5

---
+++ Zadanie5.cpp
@@ -0,0 +1,25 @@
+﻿
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+    setlocale(0, "");
+    int num0, num1, num2;
+    num0 = 0;
+    num1 = 1;
+    while (num1 <= 34) {
+        cout << num0 << endl;
+        cout << num1 << endl;
+        num2 = num0 + num1;
+        cout << num2 << endl;
+        num0 = num2 + num1;
+        num1 = num2 + num0;
+    }
+    cout << num0 << endl;
+    
+    
+}
+
+
