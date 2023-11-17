15a6191acec46988b05b24fb28ed70340f6d6ffe
Pelmen4849 <151121401+Pelmen4849@users.noreply.github.com>
2023-11-17 10:21:18 +0300

    Задание 1

---
+++ BeloukhovPractica.cpp
@@ -0,0 +1,22 @@
+﻿//Задание 1
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(0, "");
+	cout << "Введите число которое нужно возвести в квадат и в куб" << endl;
+	int num0;
+	cin >> num0;
+	int num1;
+	int num2;
+	cout << num0  << " в квадрате: ";
+	num1 = num0 * num0;
+	cout << num1 << endl;
+	cout << num0 << " в кубе: ";
+	num0 / num0;
+	num2 = num0 * num0 * num0;
+	cout << num2 << endl; 
+	
+}
+
