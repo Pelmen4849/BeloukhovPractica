fc64c1984c139b2cba18f883e7732b18778d0d52
Pelmen4849 <151121401+Pelmen4849@users.noreply.github.com>
2023-11-17 11:39:33 +0300

    Задание 4

---
+++ Zadanie4.cpp
@@ -0,0 +1,21 @@
+﻿
+#include <iostream>
+using namespace std;
+int main()
+{
+    setlocale(0, "");
+    
+    int sum = 0;
+    for (int i = 1; i <= 150; ++i) {
+        if (i % 5 == 0 || i % 7 == 0) {
+            sum += i;
+            
+        }
+        else {
+            //что бы был
+        }
+    }
+    cout <<"Сумма чисел которые делятся на 5 или 7 равна: " << sum;
+}
+
+
