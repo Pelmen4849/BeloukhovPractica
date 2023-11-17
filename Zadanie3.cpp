baf00316b36563d7321067990dfc2a35035b0c50
Pelmen4849 <151121401+Pelmen4849@users.noreply.github.com>
2023-11-17 10:47:37 +0300

    Задание 3

---
+++ Zadanie3.cpp
@@ -0,0 +1,23 @@
+﻿
+#include <iostream>
+using namespace std;
+int main()
+{
+	setlocale(0, "");
+	cout << " Введите диапазон:" << endl;
+	int diap;
+	cin >> diap;
+	cout << " " << endl;
+	int number = -1;
+	while ( number <= diap) {
+		if (number % 2 == 0) {
+			cout << number << endl;
+			number++;
+		}
+		else {
+			number++;
+		}
+	}
+}
+
+
