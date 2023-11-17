ad60c82d18e746b872894224384e899925b48884
Pelmen4849 <151121401+Pelmen4849@users.noreply.github.com>
2023-11-17 13:07:41 +0300

    Задание 6

---
+++ Zadanie6.cpp
@@ -0,0 +1,21 @@
+﻿
+#include <iostream>
+using namespace std;
+int main()
+{
+    setlocale(0, "");
+    cout << "Введите число:" << endl;
+    int num0;
+    cin >> num0;
+    cout << "Делитель числа: " << num0 << ": ";
+    for (int i = 1; i <= num0; i++) {
+        if (num0 % i == 0) {
+            cout << i << ", ";
+        }
+
+        else {
+            //что бы был
+        }
+    }
+    cout << endl;
+}
