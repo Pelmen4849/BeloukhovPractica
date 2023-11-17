e721b9b27b37f01f8962730000952c8afb882079
Pelmen4849 <151121401+Pelmen4849@users.noreply.github.com>
2023-11-17 10:35:52 +0300

    Задание 2

---
+++ BelouhovZadanie1.cpp
@@ -0,0 +1,17 @@
+﻿
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(0, "");
+    cout << "Введите два значения для которых надо найти среднее арифмитическое" << endl;
+    int midznachdel = 2;
+    int first, second;
+    cin >> first;
+    cin >> second;
+    double midznach = (first + second) / 2;
+    cout << "Среднее арифметическое " << first << " и " << second << ": " << midznach << endl;
+}
+
