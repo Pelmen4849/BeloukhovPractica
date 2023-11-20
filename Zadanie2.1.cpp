2bd58137472be3b7bd48e3c17f69ea72b39f8c17
Pelmen4849 <151121401+Pelmen4849@users.noreply.github.com>
2023-11-20 09:45:08 +0300

    Задание 1

---
+++ Zadanie2.1.cpp
@@ -0,0 +1,27 @@
+﻿
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	setlocale(0, "");
+	int first, second, res;
+	cout << "Введите два числа которые надо умножить друг на друга: " << endl;
+	cin >> first;
+	cin >> second;
+	cout << " " << endl;
+	cout << "Введите ваш результат умножения: ";
+	cin >> res;
+	if (res == first * second) {
+		cout << " " << endl;
+		cout << "Правильно" << endl;
+	}
+	else {
+		cout << " " << endl;
+		cout << "Неверно" << endl;
+		cout << "Правильный вариант: " << endl;
+		cout << first * second;
+	}
+}
+
+
